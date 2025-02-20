7e67e3c30fad397f627fd01571dd8f76df406202
GABRIEL SANTOS COUTINHO <gabriel.scoutinho4@senacsp.edu.br>
2025-02-20 20:55:39 -0300

    Atualização

--- service-worker.js
+++ service-worker.js
@@ -16,19 +16,21 @@ self.addEventListener('install', (event) => {
     );
 });
 
+// Ativação
 self.addEventListener('activate', (event) => {
-    console.log("Ativando o service worker...");
-    return self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
+    console.log("Ativando o service worker...", event)
+    return self.clients.claim()
+ })
+ // Interceptação (solicitações https servindo em cache quando off-line)
+ self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-        .then((response) => {
-            if (response) {
-                return response;
-            }
-            return fetch(event.request);
-        })
-    );
-});
+            .then((response) => {
+                if (response) {
+                    return response
+                } else {
+                    return fetch(event.request)
+                }
+            })
+    )
+ })
